ae113075e05065db117a7b133bf9439b7c58d5fa
Fidetro <zykzzzz@hotmail.com>
2024-11-27 00:30:31 +0800

    support custom default value

--- Sources/BetterCodable/CustomDefaultValue.swift
+++ Sources/BetterCodable/CustomDefaultValue.swift
@@ -32,3 +32,60 @@ public struct DefaultZeroFloatStrategy: FloatCodableStrategy {
 }
 
 public typealias DefaultZeroFloat = DefaultCodable<DefaultZeroFloatStrategy>
+
+
+@propertyWrapper
+public struct Default<T: Decodable> {
+    public var wrappedValue: T
+
+    public init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        guard let value = try? container.decode(T.self) else {
+            guard let stringValue = try? container.decode(String.self) else {
+                self.wrappedValue = Self.defaultValue
+                return
+            }
+            switch T.self {
+            case is Int.Type:
+                if let tmpValue = Int(stringValue) {
+                    self.wrappedValue = tmpValue as! T
+                    return
+                }
+            case is Double.Type:
+                if let tmpValue = Double(stringValue) {
+                    self.wrappedValue = tmpValue as! T
+                    return
+                }
+            default:
+                fatalError("Unsupported type")
+            }
+            self.wrappedValue = Self.defaultValue
+            return
+        }
+        self.wrappedValue = value
+
+        
+    }
+
+    private static var defaultValue: T {
+        switch T.self {
+        case is Int.Type:
+            return 0 as! T
+        case is String.Type:
+            return "" as! T
+        case is Double.Type:
+            return 0 as! T
+        case is Bool.Type:
+            return false as! T
+        default:
+            fatalError("Unsupported type")
+        }
+    }
+}
+
+extension Default: Decodable {}
